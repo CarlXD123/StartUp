e579cedc1feb0c894bcb05fae6b85ea0595aea86
CarlXD123 <cnavarroc@autonoma.edu.pe>
2023-07-03 00:03:17 -0500

    ddd

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Template from "./template/Template";
+import ProductDetail from "./products/detail/ProductDetail";
+import { Switch, Route } from "react-router-dom";
+import ProductList from "./products/ProductList";
+import Landing from "./landing/Landing";
+import Logueo from "./login/Logueo";
+
+
+function App() {
+  return (
+   
+      <Switch>
+        <Route path="/" exact>
+        <Logueo />
+        </Route>
+        <Route path="/product" exact>
+          <ProductList />
+        </Route>
+        <Route path="/products/:slug">
+          <ProductDetail />
+        </Route>
+        <Route path="/inicio" exact>
+          <Landing />
+        </Route>
+      </Switch>
+    
+  );
+}
+
+export default App;
